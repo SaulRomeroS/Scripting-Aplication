660e0a65af1a091cc0fc640664009534e1d11bc9
Saulrs98 <A01351663@itesm.mx>
2023-08-30 15:27:51 -0600

    Final commit- Main modifications

--- main.py
+++ main.py
@@ -4,9 +4,10 @@
 import csv
 
 
-# data = [{'name': 'John Doe', 'age': 30}, {'name': 'Jane Doe', 'age': 25}]
+
 def write_list_of_dicts_to_csv(filename, data):
-    with open(filename, 'w') as f:
+    with open(filename, 'w', newline='') as f:
+        fieldnames = data[0].keys()
         writer = csv.DictWriter(f, fieldnames=data[0].keys())
         writer.writeheader()
         writer.writerows(data)
@@ -18,12 +19,21 @@ def read_csv_to_dict(filename):
         return list(reader)
 
 
-def main(filename):
-    data = read_csv_to_dict(filename)
-    for row in data:
-        print(row)
+def main(source_filename, batch_filename, output_filename):
+    source_data = read_csv_to_dict(source_filename)
+    batch_data = read_csv_to_dict(batch_filename)
+    inventory = {item['SKU']: item for item in source_data}
+    
+    for item in batch_data:
+        sku = item['SKU']
+        if sku in inventory:
+            inventory[sku]['Quantity'] = str(int(inventory[sku]['Quantity']) + int(item['Quantity']))
+        else:
+            inventory[sku] = item
+    
+    write_list_of_dicts_to_csv(output_filename, list(inventory.values()))
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    main('sample_grocery.csv')
+    main('sample_grocery.csv', 'grocery_batch_1.csv', 'grocery_db.csv')
